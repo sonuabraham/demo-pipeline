b34e8dbcfe6cb647c48c904cca1fa8f6a0e8d7ae
Sonu Abraham <sonu.abraham@rfi.com.au>
2023-10-27 00:01:05 +1100

    define initial pipeline

--- bin/demo-pipeline.ts
+++ bin/demo-pipeline.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { DemoPipelineStack } from '../lib/demo-pipeline-stack';
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { DemoPipelineStack } from "../lib/demo-pipeline-stack";
 
 const app = new cdk.App();
-new DemoPipelineStack(app, 'DemoPipelineStack', {
+new DemoPipelineStack(app, "DemoPipelineStack", {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -15,7 +15,7 @@ new DemoPipelineStack(app, 'DemoPipelineStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  env: { account: "104612892635", region: "us-east-1" },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
--- lib/demo-pipeline-stack.ts
+++ lib/demo-pipeline-stack.ts
@@ -1,16 +1,17 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
-
+import {CodePipeline,CodePipelineSource,ShellStep} from "aws-cdk-lib/pipelines";
 export class DemoPipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'DemoPipelineQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const pipeline = new CodePipeline(this,"Pipeline",{
+      pipelineName:"DemoPipeline",
+      synth:new ShellStep("Synth",{
+        input: CodePipelineSource.gitHub("sonuabraham/demo-pipeline","main"),
+        commands:["npm ci","npm run build","npx cdk synth"],
+      });
+    });
+  
   }
 }
