52e46a75850b5cc8a5fd3ab699e16087020bc1e2
Sonu Abraham <sonu.abraham@rfi.com.au>
2023-10-27 00:04:32 +1100

    fixed compilation error

--- lib/demo-pipeline-stack.ts
+++ lib/demo-pipeline-stack.ts
@@ -1,17 +1,20 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import {CodePipeline,CodePipelineSource,ShellStep} from "aws-cdk-lib/pipelines";
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import {
+  CodePipeline,
+  CodePipelineSource,
+  ShellStep,
+} from "aws-cdk-lib/pipelines";
 export class DemoPipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const pipeline = new CodePipeline(this,"Pipeline",{
-      pipelineName:"DemoPipeline",
-      synth:new ShellStep("Synth",{
-        input: CodePipelineSource.gitHub("sonuabraham/demo-pipeline","main"),
-        commands:["npm ci","npm run build","npx cdk synth"],
-      });
+    const pipeline = new CodePipeline(this, "Pipeline", {
+      pipelineName: "DemoPipeline",
+      synth: new ShellStep("Synth", {
+        input: CodePipelineSource.gitHub("sonuabraham/demo-pipeline", "main"),
+        commands: ["npm ci", "npm run build", "npx cdk synth"],
+      }),
     });
-  
   }
 }
