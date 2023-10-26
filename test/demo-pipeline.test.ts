99eb29f95264482a96317120387ea9251758384c
Sonu Abraham <sonu.abraham@rfi.com.au>
2023-10-26 23:46:32 +1100

    initial commit

---
+++ test/demo-pipeline.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DemoPipeline from '../lib/demo-pipeline-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/demo-pipeline-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DemoPipeline.DemoPipelineStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
