544d99a4d8c7d199b0742dc3bc316e2dd4dba906
Sonu Abraham <sonu.abraham@rfi.com.au>
2023-10-27 00:55:17 +1100

    fixed typo error

--- lambda/node.js
+++ lambda/node.js
@@ -1,15 +1,15 @@
-exports handler = async function(event){
-    console.log("request:",JSON.stringify(event,undefined,2));
+exports.handler = async function (event) {
+  console.log("request:", JSON.stringify(event, undefined, 2));
 
-    json_data = {
-        account: `${process.env.acctid.replace(/\d(?=\d{4})/g,"*")}`,
-        region:`${process.env.region}`,
-    };
-    return {
-        statusCode:200,
-        headers:{
-            "Content-Type":"application/json",
-        },
-        body:JSON.stringify(json_data),
-        };
- };
+  json_data = {
+    account: `${process.env.acctid.replace(/\d(?=\d{4})/g, "*")}`,
+    region: `${process.env.region}`,
+  };
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(json_data),
+  };
+};
